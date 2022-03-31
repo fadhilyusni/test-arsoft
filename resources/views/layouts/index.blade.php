0818870d71c13e20152208919db5360c464025c4
fadhilyusni <42725126+fadhilyusni@users.noreply.github.com>
2022-03-31 14:35:17 +0700

    Initial commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,244 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Test Arsoft</title>
+
+    <!-- Custom styles for this template -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .circle {
+            width:30px;
+            height:30px;
+            border-radius:50%;
+            background-color:green;
+        }
+        .badge{
+            background-color:orange;
+            color:white;
+            padding-top:8px;
+            padding-left:20px;
+            padding-right:20px;
+            padding-bottom:8px;
+            margin-bottom:10px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content p-3">
+    <div class="row justify-content-around">
+        <div class="col">
+            <h2 class="mb-4">
+                <strong>List Tugas Harian</strong>
+            </h2>
+            <p class="text-muted">
+                klik tombol <button class="btn btn-primary brn-sm" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">+Tugas Baru</button>
+                </button> untuk membuat tugas harian baru
+            </p>
+            <p class="text-muted">
+                jangan lupa untuk memeriksa tugas anda
+            </p>
+</div>
+        </div>
+        <div class="col-ml-auto">
+            <div class="row">
+                <div class="col">
+                    <h6 class="mt-4">
+                        Tampilkan Tugas yang
+                    </h6>
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                        <label class="btn btn-outline-primary" for="btnradio1">Belum selesai</label>
+
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio2">Sudah selesai</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <h6 class="mt-4">
+                        Urut Bedasarkan
+                    </h6>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="tugas py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="circle mb-3">
+                        </div>
+                    </div>
+                    <span class="badge">01 january 2022</span>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="alert alert-success" role="alert">
+                            Succes
+                        </div>
+                        <small class="text-muted">1 tahun yang lalu</small>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="circle mb-3">
+                        </div>
+                    </div>
+                    <span class="badge">01 january 2022</span>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="alert alert-success" role="alert">
+                            Succes
+                        </div>
+                        <small class="text-muted">1 tahun yang lalu</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="circle mb-3">
+                        </div>
+                    </div>
+                    <span class="badge">01 january 2022</span>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="alert alert-success" role="alert">
+                            Succes
+                        </div>
+                        <small class="text-muted">1 tahun yang lalu</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="circle mb-3">
+                        </div>
+                    </div>
+                    <span class="badge">01 january 2022</span>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="alert alert-success" role="alert">
+                            Succes
+                        </div>
+                        <small class="text-muted">1 tahun yang lalu</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="circle mb-3">
+                        </div>
+                    </div>
+                    <span class="badge">01 january 2022</span>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="alert alert-success" role="alert">
+                            Succes
+                        </div>
+                        <small class="text-muted">1 tahun yang lalu</small>
+                    </div>
+                </div>
+                
+            </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- modal -->
+<div class="modal fade" id="exampleModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Urutkan bedasarkan</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="customCheck1">
+            <label class="from-check-label" for="flexCheckIndeterminate">Tanggal Dibuat (Terbaru)</label>
+            </div>
+        </div>
+        <div class="dropdown-divider"></div>
+        <div class="modal-body">
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="customCheck1">
+            <label class="from-check-label" for="flexCheckIndeterminate">Tanggal Dibuat (Terbaru)</label>
+            </div>
+        </div>
+        <div class="dropdown-divider"></div>
+        <div class="modal-body">
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="customCheck1">
+            <label class="from-check-label" for="flexCheckIndeterminate">Tanggal Dibuat (Terbaru)</label>
+            </div>
+        </div>
+        <div class="dropdown-divider"></div>
+        <div class="modal-body">
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="customCheck1">
+            <label class="from-check-label" for="flexCheckIndeterminate">Tanggal Dibuat (Terbaru)</label>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+<!-- off canvas -->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+        <h5 id="offcanvasRightLabel">Tambahkan Tugas Baru</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div class="mb-3">
+        <label for="date" class="col-1 col-form-label">Date</label>
+        <div class="input-group date" id="datepicker">
+        <input type="text" class="form-control" id="date"/>
+        <span class="input-group-append">
+            <span class="input-group-text bg-light d-block">
+            <i class="fa fa-calendar"></i>
+            </span>
+        </span>
+        </div>
+        <div class="mb-3">
+        <label for="exampleColorInput" class="form-label">Color picker</label>
+        <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Keterangan Tugas</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
+
+</body>
+</html>
